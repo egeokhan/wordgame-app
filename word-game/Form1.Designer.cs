cf4bc540cffb39c27d99e5275978c00cf9287ae4
Ege Okhan <95880067+egeokhan@users.noreply.github.com>
2021-12-22 18:34:53 +0300

    ...

---
+++ word-game/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace word_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(12, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(407, 292);
+            this.listBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.Location = new System.Drawing.Point(12, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(407, 33);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Value = 100;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(168, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(8, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(431, 425);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Question Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ word-game/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace word_game
+{
+    public partial class Form1 : Form
+    {
+        string[] questions =
+            {
+            "Organs of our body?",
+            "Capital of countries?",
+            "Popular Turkish dishes?",
+            "Phone manufacturer brands?",
+            "European countries?"
+            };
+        int i = 0;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            progressBar1.ForeColor = Color.Blue;
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Question1();
+                Question2();
+                Question3();
+                Question4();
+                Question5();
+            }
+        }
+
+        private void Question5()
+        {
+            string path = Path.Combine(Application.StartupPath, "question5.txt");
+            string[] answers = File.ReadAllLines(path);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (label1.Text == questions[4] && (textBox1.Text).ToLower() == answers[i].ToLower())
+                {
+                    if (listBox1.Items.Contains(answers[i]) == false)
+                    {
+                        listBox1.Items.Add(answers[i]);
+                        score += 10;
+                    }
+                }
+            }
+            textBox1.Clear();
+        }
+
+        private void Question4()
+        {
+            string path = Path.Combine(Application.StartupPath, "question4.txt");
+            string[] answers = File.ReadAllLines(path);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (label1.Text == questions[3] && (textBox1.Text).ToLower() == answers[i].ToLower())
+                {
+                    if (listBox1.Items.Contains(answers[i]) == false)
+                    {
+                        listBox1.Items.Add(answers[i]);
+                        score += 10;
+                    }
+                }
+            }
+            textBox1.Clear();
+        }
+
+        private void Question3()
+        {
+            string path = Path.Combine(Application.StartupPath, "question3.txt");
+            string[] answers = File.ReadAllLines(path);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (label1.Text == questions[2] && (textBox1.Text).ToLower() == answers[i].ToLower())
+                {
+                    if (listBox1.Items.Contains(answers[i]) == false)
+                    {
+                        listBox1.Items.Add(answers[i]);
+                        score += 10;
+                    }
+                }
+            }
+            textBox1.Clear();
+        }
+
+        private void Question2()
+        {
+            string path = Path.Combine(Application.StartupPath, "question2.txt");
+            string[] answers = File.ReadAllLines(path);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (label1.Text == questions[1] && (textBox1.Text).ToLower() == answers[i].ToLower())
+                {
+                    if (listBox1.Items.Contains(answers[i]) == false)
+                    {
+                        listBox1.Items.Add(answers[i]);
+                        score += 10;
+                    }
+                }
+            }
+            textBox1.Clear();
+        }
+
+        private void Question1()
+        {
+            string path = Path.Combine(Application.StartupPath, "question1.txt");
+            string[] answers = File.ReadAllLines(path);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (label1.Text == questions[0] && (textBox1.Text).ToLower() == answers[i].ToLower())
+                {
+                    if (listBox1.Items.Contains(answers[i]) == false)
+                    {
+                        listBox1.Items.Add(answers[i]);
+                        score += 10;
+                    }
+                }
+            }
+            textBox1.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Next";
+            progressBar1.Value = 100;
+            timer1.Enabled=true;
+            listBox1.Items.Clear();
+            WriteQuestion();
+        }
+
+        private void WriteQuestion()
+        {
+            if(i == 4)
+            {
+                button1.Text = "Finish";
+            }
+            if(i >= 5)
+            {
+                MessageBox.Show($"Game Over! Score: {score}");
+                this.Close();
+                i = 0;
+            }
+            label1.Text = questions[i];
+            i++;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Time();
+        }
+
+        private void Time()
+        {
+            progressBar1.Value -= 5;
+            if (progressBar1.Value == 0)
+            {
+                timer1.Stop();
+                progressBar1.Value = 100;
+            }
+        }
+    }
+}
